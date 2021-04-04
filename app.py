937a1169735ea3d4a39a4f07a3edff29846c4283
KueZie <huntergoram2@gmail.com>
2021-04-04 00:54:34 -0400

    Test

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from markupsafe import escape
+import dotenv
+
+dotenv.load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'id': ''})
+@app.route('/<id>')
+def hello_world(id=''):
+    return f'Hello, Hunter! {id}'
+
+@app.route('/home')
+def home():
+    return 'Home'
