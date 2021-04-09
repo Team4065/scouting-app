a94190318b453588818ac3edac3f8501c4ac05ef
KueZie <huntergoram2@gmail.com>
2021-04-09 12:48:07 -0400

    Added admin sidebar with placeholders

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
 
+from ..auth.validators import allow_if
 from . import admin_blueprint as admin
 
 @admin.route('/')
+@allow_if(['admin'])
 def dashboard():
   return render_template('admin.html')
