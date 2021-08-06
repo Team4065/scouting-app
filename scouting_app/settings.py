7e257cba4300a955b1f1da219b4d2e9d9266aabd
KueZie <huntergoram2@gmail.com>
2021-08-06 02:14:41 -0400

    Switch to postgresql

--- scouting_app/settings.py
+++ scouting_app/settings.py
@@ -9,23 +9,20 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+DEFAULT_KEY = 'django-insecure-2-g7dl*bl4mv*n0jy)3$fp9v*vvmbi!9$o6-p@-z3!wg#==gx%'
+SECRET_KEY = os.getenv('SECRET_KEY', DEFAULT_KEY)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2-g7dl*bl4mv*n0jy)3$fp9v*vvmbi!9$o6-p@-z3!wg#==gx%'
+DEBUG = os.getenv('DEBUG', False)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
+CORS_ORIGIN_WHITELIST = ['http://localhost:8000']
 
 
 # Application definition
@@ -37,6 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'scouting_app',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'scouting_app.urls'
@@ -71,13 +72,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'scouting_app.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'HOST': '',
+        'PORT': '',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
